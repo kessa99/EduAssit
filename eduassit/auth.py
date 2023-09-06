c533e5776fafdf10670841fed2cd19e8618f7c3c
kessa99 <108158840+kessa99@users.noreply.github.com>
2023-09-06 19:42:28 +0000

    end of html session

--- eduassit/auth.py
+++ eduassit/auth.py
@@ -15,14 +15,17 @@ def logout():
 @auth.route("/sign-up", methods = ['GET', 'POST'])
 def signup():
     if request.method == 'POST':
+        userName = request.form.get('Username')
+        name = request.form.get('name')
         email = request.form.get('email')
-        firstName = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
         if len(email) < 4:
             flash('Email mst be greater than 3 characters.', category='error')
-        elif len(firstName) < 2:
+        elif len(userName) < 2:
+            flash('First Name must be greater than 1 characters.', category='error')
+        elif len(name) < 2:
             flash('First Name must be greater than 1 characters.', category='error')
         elif password1 is None or password2 is None:
             flash('Password fields are required.', category='error')
